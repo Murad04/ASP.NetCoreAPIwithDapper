71e3bdf7c00bf76aa9576ce75d8238320283a031
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-05 11:05:11 +0400

    Added AppContext,Models and service

---
+++ ASP.NetCoreAPIwithDapper/Models/Parameters.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NetCoreAPIwithDapper.Models
+{
+    public class Parameters
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+    }
+}
--- ASP.NetCoreAPIwithDapper/Program.cs
+++ ASP.NetCoreAPIwithDapper/Program.cs
@@ -1,8 +1,16 @@
+using ASP.NetCoreAPIwithDapper.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ASP.NetCoreAPIwithDapper.DataContext.AppContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IDapper, ASP.NetCoreAPIwithDapper.Services.Dapper>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,8 +24,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();

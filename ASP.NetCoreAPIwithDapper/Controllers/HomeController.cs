9584f2a14cbad9c07f48ef24dcfa7a7fee366e07
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-05 10:35:26 +0400

    Add project files.

---
+++ ASP.NetCoreAPIwithDapper/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCoreAPIwithDapper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
